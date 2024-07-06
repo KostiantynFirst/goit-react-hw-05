9eaff808d7cf0a9cb2405d43c9603742d16c4196
Gaudi <goncharov.kostiantyn@gmail.com>
2024-07-06 16:19:25 +0300

    add pages and components

---
+++ src/movie-api.jsx
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const MOVIEKEY = process.env.REACT_APP_CAT_API_KEY;
+
+export const getTrendingMovies = async () => {
+
+  try {
+    const res = await axios.get(
+      `trending/movie/day?`, {
+          params: {
+              api_key: MOVIEKEY,
+          }
+      }
+    );
+   return res;
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+export const getMoviesInfo = async (movieId) => {
+  try {
+    const res = await axios.get(`movie/${movieId}`, {
+      params: {
+        api_key: MOVIEKEY,
+      }
+    });
+    return res;
+  
+  } catch (error) {
+    console.log('error', error);
+}
+}
+
+export const getMovieSearch = async (search) => {
+  try {
+    const res = await axios.get(`search/movie`, {
+      params: {
+        api_key: MOVIEKEY,
+        language: 'en-US',
+        query: search,
+        include_adult: false,
+        page: 1,
+      },
+    });
+    return res;
+    
+  } catch (error) {
+      console.log('error', error);
+  }
+}
+
+export const getCast = async (movieId) => {
+  try {
+    const res = await axios.get(`movie/${movieId}/credits`, {
+      params: {
+        api_key: MOVIEKEY,
+        language: 'en-US',
+      },
+    });
+    return res;
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+export const getReviews = async (movieId) => {
+  try {
+    const res = await axios.get(`movie/${movieId}/reviews`, {
+      params: {
+        api_key: MOVIEKEY,
+        language: 'en-US',
+        page: 1,
+      }
+    });
+    return res;
+  } catch (error) {
+    console.log('error', error);
+  }
+}
